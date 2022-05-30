cea644f2cd8e1baf9c1114054bceef756e3d2250
Ben De Meurichy <ben.demeurichy@gmail.com>
2022-05-30 10:20:40 +0200

    another one

--- blok juni/sorteertrucje.py
+++ blok juni/sorteertrucje.py
@@ -14,19 +14,19 @@ class Rooster:
         lengte = len(lijst[0]) if len(lijst[0]) > 0 else -1
         assert all(len(el) == lengte for el in lijst) and \
                all(isinstance(item, int) for sublist in lijst for item in sublist), "ongeldig rooster"
-        self.rooster = lijst
+        self.veld = lijst
 
     def grootste(self):
-        m = self.rooster[0][0]
-        for el in self.rooster:
+        m = self.veld[0][0]
+        for el in self.veld:
             for i in el:
                 if i > m:
                     m = i
         return m
 
     def kleinste(self):
-        m = self.rooster[0][0]
-        for el in self.rooster:
+        m = self.veld[0][0]
+        for el in self.veld:
             for i in el:
                 if i < m:
                     m = i
@@ -35,20 +35,20 @@ class Rooster:
     def __str__(self):
         return "\n".join([" ".join([
             f"{str(i): >{len(str(self.grootste())) if len(str(self.grootste())) > len(str(self.kleinste())) else len(str(self.kleinste()))}}"
-            for i in el]) for el in self.rooster])
+            for i in el]) for el in self.veld])
 
     def gesorteerd(self, dalend=False, kolommen=False):
         if kolommen:
-            for i, _ in enumerate(self.rooster[0]):
+            for i, _ in enumerate(self.veld[0]):
                 kolom = []
-                for el, _ in enumerate(self.rooster):
-                    kolom.append(self.rooster[el][i])
+                for el, _ in enumerate(self.veld):
+                    kolom.append(self.veld[el][i])
                 verg = kolom[:]
                 verg.sort(reverse=dalend)
                 if kolom != verg:
                     return False
         else:
-            for el in self.rooster:
+            for el in self.veld:
                 verg = el[:]
                 verg.sort(reverse=dalend)
                 if el != verg:
@@ -58,13 +58,13 @@ class Rooster:
     def sorteer(self, dalend=False, kolommen=False):
         raster = []
         if kolommen:
-            for i, _ in enumerate(self.rooster[0]):
+            for i, _ in enumerate(self.veld[0]):
                 kolom = []
-                for el, _ in enumerate(self.rooster):
-                    kolom.append(self.rooster[el][i])
+                for el, _ in enumerate(self.veld):
+                    kolom.append(self.veld[el][i])
                 raster.append(kolom)
         else:
-            raster = self.rooster
+            raster = self.veld
 
         for el in raster:
             el.sort(reverse=dalend)
@@ -77,20 +77,20 @@ class Rooster:
                     kolom.append(raster[el][i])
                 temp.append(kolom)
             raster = temp
-        self.rooster = raster
+        self.veld = raster
         return self
 
     def __add__(self, other):
-        assert len(self.rooster) == len(other.rooster), "aantal rijen is verschillend"
+        assert len(self.veld) == len(other.veld), "aantal rijen is verschillend"
         roost = []
-        for i, el in enumerate(self.rooster):
+        for i, el in enumerate(self.veld):
             temp = el[:]
-            temp.extend(other.rooster[i])
+            temp.extend(other.veld[i])
             roost.append(temp)
         return Rooster(roost)
 
     def __sub__(self, other):
-        assert len(self.rooster[0]) == len(other.rooster[0]), "aantal kolommen is verschillend"
-        roost = self.rooster[:]
-        roost.extend(other.rooster)
+        assert len(self.veld[0]) == len(other.veld[0]), "aantal kolommen is verschillend"
+        roost = self.veld[:]
+        roost.extend(other.veld)
         return Rooster(roost)
