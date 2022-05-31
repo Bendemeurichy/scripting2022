0591ef84f34514ef34a66b953faf1aacfa5e6667
Ben De Meurichy <ben.demeurichy@gmail.com>
2022-05-31 17:57:33 +0200

    31/5afternoon

--- blok_juni/bitcoin.js
+++ blok_juni/bitcoin.js
@@ -1,38 +1,38 @@
 // https://dodona.ugent.be/nl/courses/27/series/463/activities/15429256/#
 
 function winst (val, proc) {
-  const error = { name: 'AssertionError', message: 'ongeldige acties' }
-  const procArr = proc.split('')
-  let res = 0
-  let owned = 0
+  const error = { name: 'AssertionError', message: 'ongeldige acties' };
+  const procArr = proc.split('');
+  let res = 0;
+  let owned = 0;
   for (const i in procArr) {
     if (procArr[i] === 'K') {
-      owned++
+      owned++;
       if (owned !== 1) {
-        throw error
+        throw error;
       }
-      res -= val[i]
+      res -= val[i];
     } else if (procArr[i] === 'V') {
-      res += val[i]
-      owned--
+      res += val[i];
+      owned--;
       if (owned !== 0) {
-        throw error
+        throw error;
       }
     }
   }
   if (owned !== 0) {
-    throw error
+    throw error;
   }
-  return res
+  return res;
 }
 
 function maximaleWinst (val) {
-  let res = 0
-  let pacc = -val[0]
+  let res = 0;
+  let pacc = -val[0];
   for (let i = 1; i < val.length; i++) {
-    const p = res
-    res = Math.max(res, pacc + val[i])
-    pacc = Math.max(pacc, p - val[i])
+    const p = res;
+    res = Math.max(res, pacc + val[i]);
+    pacc = Math.max(pacc, p - val[i]);
   }
-  return res
+  return res;
 }
